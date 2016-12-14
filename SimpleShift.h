4fa56ea84f59470a078d41c9b69b5c09d9eb539d
dimecoin <dimecoin@users.noreply.github.com>
2016-12-13 18:32:49 -0600

    Update SimpleShift.h

--- SimpleShift.h
+++ SimpleShift.h
@@ -18,33 +18,38 @@ class SimpleShift {
 		 * SCKPin is Arduino pin number that is connected to SRCLK [pin 11] on 74HC595
 		 * SRCLRPin : 
 		 * 	For hardware mode: this is Arduino pin number connected to to !SRCLRPIN [pin 10] on 74HC595.
-		 * 	For software mode:  Use -1 in constructor and tie !SRLCPIN [pin 10] on 74HC595 to VCC.
+		 * 	For software mode:  Use -1 in constructor and tie !SRCLR [pin 10] on 74HC595 to VCC.
 		 * 	Do not float this pin.
 		 */
 		SimpleShift (uint8_t numberOfShiftRegisters, uint8_t SERPin, uint8_t RCKPin, uint8_t SCKPin, int8_t SRCLRPin);
 		~SimpleShift ();
 
-		/** Zero out all bits in memory buffer*/
+		/** Zero out all bits in memory buffer.  
+		* Requires a writeRegisters() call to take affect on shift register[s].*/
 		void clearBuffer();
 
 		/** Instantly clears shift register and does NOT require a writeRegisters() to take affect.  Uses hardware if SRCLRPin is wired, otherwise uses software mode.
-		 * clearBuffer:  You can either clear your memory buffer or not. 
+		 * clearBuffer:  You can either clear your memory buffer or not.   
 		 */
 		void clearShiftRegister(bool wipeBuffer);
 		
-		/** Set all bits to 1  in memory buffer*/
+		/** Set all bits to 1  in memory buffer
+		* Requires a writeRegisters() call to take affect on shift register[s].*/
 		void fillBuffer();
 
-		/** Set random junk to memory buffer */
+		/** Set random junk to memory buffer 
+		* Requires a writeRegisters() call to take affect on shift register[s].*/
 		void randomFillBuffer();
 
-		/** Invert bits (bitewise opertation).  That is set all 0's to 1 and visa-versa */
+		/** Invert bits (bitewise opertation).  That is set all 0's to 1 and visa-versa 
+		* Requires a writeRegisters() call to take affect on shift register[s].*/
 		void invertBuffer();
 
-		/** Set a single bit */
+		/** Set a single bit 
+		* Requires a writeRegisters() call to take affect on shift register.*/
 		void setBufferBit(uint8_t index, uint8_t value);
 
-		/** Flushes memory buffer to Shift Registers.  This must been called after all write operations. */	
+		/** Flushes memory buffer to Shift Registers.  This MUST been called after all memory/buffer operations. The only exception is clearShiftRegister(bool).*/	
 		void writeRegisters();
 
 		/** Get bit in memory buffer, not necessarily what is on shift register.  It will differ is writeRegisters() hasn't been called yet. */
